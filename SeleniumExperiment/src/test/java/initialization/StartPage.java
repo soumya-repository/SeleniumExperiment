c7a57ad07debb000341c0153fe545a59d86d2b7e
Soumya Ranjan Patri <way2s@soumya>
2021-12-26 21:45:23 +0530

    initial commit

---
+++ SeleniumExperiment/src/test/java/initialization/StartPage.java
@@ -0,0 +1,20 @@
+package initialization;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class StartPage {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.google.co.in");
+		
+		//driver.close();
+
+	}
+
+}
